e03b0484cdbfab7a4658299897823dd3dc47c51f
jhmin <hmin5015@gmail.com>
2022-12-12 22:28:53 -0800

    add println and variables and comments

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -6,15 +6,24 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name:String
+    private var mobile:String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            var name = nameInput.text.toString()
-            var mobile = mobileInput.text.toString()
+            name = nameInput.text.toString()
+            mobile = mobileInput.text.toString()
 
+            /*
+                Show message in Toast
+            * */
             Toast.makeText(applicationContext, "이름: $name, 전화번호: $mobile", Toast.LENGTH_LONG).show()
+
+            // show value in Logcat using println
+            println("안녕 : ${name}")
         }
     }
 }
