a3885741c73cb3357ce17995db5fd20ecc462231
jhmin <hmin5015@gmail.com>
2022-12-12 19:45:53 -0800

    remove unnecessary codes

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,22 +1,11 @@
 package com.example.hello
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        button.setOnClickListener {
-            Toast.makeText(applicationContext, "버튼이 눌렸어요", Toast.LENGTH_SHORT).show()
-        }
-
-        button2.setOnClickListener {
-            val input1 = input1.text.toString()
-            output1.text = "결과: $input1"
-        }
     }
 }
