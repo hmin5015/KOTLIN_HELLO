fb0ec9c0861af13ffa84624f196c3ff9f2d5e395
jhmin <hmin5015@gmail.com>
2022-12-12 21:10:09 -0800

    remove previous sample and add name and phone

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.hello
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            var name = nameInput.text.toString()
+            var mobile = mobileInput.text.toString()
+
+            Toast.makeText(applicationContext, "이름: $name, 전화번호: $mobile", Toast.LENGTH_LONG).show()
+        }
     }
 }
