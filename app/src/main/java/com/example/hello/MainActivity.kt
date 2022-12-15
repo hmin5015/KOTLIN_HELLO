380d157ccd351761f8d4fa9439797b0cc0166b9b
jhmin <hmin5015@gmail.com>
2022-12-14 20:50:52 -0800

    remove previous codes and add show function

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -2,48 +2,19 @@ package com.example.hello
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.hello.constants.mBonus
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var first:Int = 0
-    var second:Int = 0
-
-    companion object {
-        const val BONUS = 100
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addButton.setOnClickListener {
-            val firstStr = input1.text.toString()
-            val secondStr = input2.text.toString()
-
-            first = firstStr.toInt()
-            second = secondStr.toInt()
-
-            val result = first + second + MainActivity.BONUS + mBonus
-            output1.text = "결과 : $result"
-
-            if (result == 20) {
-                output1.text = "더하기 결과가 20 입니다."
-            }
-
-            if (firstStr == "10") {
-                output1.append("\n첫번째 입력된 값이 10입니다.")
-            }
-
-            val input1:Any = "안녕"
-            val input2:Any = 10
-
-            if (input1 is String) {
-                val output2:String = input1 as String
-                output1.text = "input1 은 문자열 자료형입니다."
-            }
-
-            val output3:String? = input1 as String?
+        button.setOnClickListener {
+            show("안녕")
         }
+    }
+
+    fun show(message:String) {
+        println("버튼이 눌렸어요. : $message")
     }
 }
